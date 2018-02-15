ecc7e7e1ad73ea416693278bc7cd0aca2962305e
Leah Birch <goldbeat98@gmail.com>
2018-02-15 12:55:38 +0200

    added background

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ let footerHTML = `
 	
 	<p>Floral background <a href="https://www.freepik.com/free-vector/abstract-floral-background_789164.htm">Designed by Freepik</a></p>
 	
-	<div>Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> and <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	<p>Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> and <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
 	
 	<p>&copy; TutorHouse</p>
 `;
