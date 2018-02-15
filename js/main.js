bb5d3e2bdc0a960ebd4f3f6572f6e5fc4d2440d2
Leah Birch <goldbeat98@gmail.com>
2018-02-15 12:13:12 +0200

    added icons and changed layout

--- js/main.js
+++ js/main.js
@@ -43,6 +43,11 @@ function initMap() {
 const footer = document.getElementById('footer');
 let footerHTML = `
 	<a class="call" href="tel:0218243336">CALL: 021 824 3336</a>
+	
+	<p>Floral background <a href="https://www.freepik.com/free-vector/abstract-floral-background_789164.htm">Designed by Freepik</a></p>
+	
+	<div>Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> and <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	
 	<p>&copy; TutorHouse</p>
 `;
 
