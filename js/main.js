23fd27686b2879a71454f757304aed037d0a2eb7
Leah Birch <goldbeat98@gmail.com>
2018-02-15 14:45:41 +0200

    added sticky.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
 //js
 
-/*MENU
-
-const header = document.getElementById('header');
-let headerHTML = `
-	<ul id="menu">
-		<a href="index.html"><li>Home</li></a>
-		<a href="services.html"><li>Services</li></a>
-		<a href="contact.html"><li>Contact</li></a>
-	</ul>
-	
-	<img id="logo" src="img/logo.jpg" alt="logo"/>
-	
-	<div>
-		<h2 class="sub-heading">Educational Institution in Bellville</h2>
-		<a class="call" href="tel:0218243336">CALL: 021 824 3336</a>
-	</div>
-	<h1 class="sub-heading-2">We specialize in Home Schooling, Tutoring and After Care for Grade R to Grade 12.</h1>
-`;
-header.innerHTML = headerHTML;
-*/
-
 //google map
 
 function initMap() {
@@ -35,13 +14,15 @@ function initMap() {
 	});
 }
 
-
-
-
+//sticky.js
+$("#menu").sticky({
+	topSpacing:0,
+	zIndex: 100
+});
 
 //FOOTER
-const footer = document.getElementById('footer');
-let footerHTML = `
+
+$('#footer').html(`
 	<a class="call" href="tel:0218243336">CALL: 021 824 3336</a>
 	
 	<p>Floral background <a href="https://www.freepik.com/free-vector/abstract-floral-background_789164.htm">Designed by Freepik</a></p>
@@ -49,6 +30,4 @@ let footerHTML = `
 	<p>Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> and <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
 	
 	<p>&copy; TutorHouse</p>
-`;
-
-footer.innerHTML = footerHTML;
+`);
